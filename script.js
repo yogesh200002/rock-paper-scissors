271ecde355e0fba9b2a1e6323b663130023d7a25
Yogesh S <yogeshvishal98@gmail.com>
2022-03-16 14:48:05 +0530

    Cleaned code

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 function computerPlay(){
-    const elements = ["rock","paper","scissors"]
+    const elements = ["rock","paper","scissors"];
     return elements[Math.floor(Math.random()*3)]
 }
 
-let winCount=0
+let winCount=0;
 
 function playRound(playerSelection,computerSelection){
     switch(playerSelection){
         case "rock":
             if(computerSelection === "paper"){
-                console.log("You Lose! Paper defeats Rock")
-                winCount>0 ? winCount -= 1 : winCount=0
+                console.log("You Lose! Paper defeats Rock");
+                winCount>0 ? winCount -= 1 : winCount=0;
                 break
             } 
             else if(computerSelection === "scissors"){
-                console.log("You Win!, Rock defeats Scissors")
-                winCount += 1
+                console.log("You Win!, Rock defeats Scissors");
+                winCount += 1;
                 break
             }
             else{
-                return console.log("Draw")
+                return console.log("Draw");
                 break
             }
         case "paper":
             if(computerSelection === "scissors"){
-                console.log("You Lose! Scissors defeats paper")
-                winCount>0 ? winCount -= 1 : winCount=0
+                console.log("You Lose! Scissors defeats paper");
+                winCount>0 ? winCount -= 1 : winCount=0;
                 break
             } 
             else if(computerSelection === "rock"){
-                console.log("You Win!, Paper defeats Rock")
-                winCount += 1
+                console.log("You Win!, Paper defeats Rock");
+                winCount += 1;
                 break
             }
             else{
-                return console.log("Draw")
+                return console.log("Draw");
                 break
             }
         case "scissors":
             if(computerSelection === "rock"){
-                console.log("You Lose! Rock defeats Scissors")
-                winCount>0 ? winCount -= 1 : winCount=0
+                console.log("You Lose! Rock defeats Scissors");
+                winCount>0 ? winCount -= 1 : winCount=0;
                 break
             } 
             else if(computerSelection === "paper"){
-                console.log("You Win!, Scissors defeats Paper")
-                winCount += 1
+                console.log("You Win!, Scissors defeats Paper");
+                winCount += 1;
                 break
             }
             else{
@@ -58,8 +58,8 @@ function playRound(playerSelection,computerSelection){
     }
 }
 for(i=0;i<5;i++){
-    let playerSelection = prompt("Rock, Paper or Scissors?")
-    playerSelection = playerSelection.toLowerCase()
+    let playerSelection = prompt("Rock, Paper or Scissors?");
+    playerSelection = playerSelection.toLowerCase();
     let computerSelection = computerPlay();
     playRound(playerSelection,computerSelection)
 }
